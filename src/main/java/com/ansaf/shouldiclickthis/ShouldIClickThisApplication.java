7c1d70c0b8c9837f92ee71a65c0900a98df82ec8
vatdaell <ansafahmad70@gmail.com>
2024-03-20 00:10:35 -0400

    add cors unblock list

--- src/main/java/com/ansaf/shouldiclickthis/ShouldIClickThisApplication.java
+++ src/main/java/com/ansaf/shouldiclickthis/ShouldIClickThisApplication.java
@@ -1,16 +1,30 @@
 package com.ansaf.shouldiclickthis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
 public class ShouldIClickThisApplication {
-
+	@Value("${unblock.list}")
+	private String[] unblockList;
 	public static void main(String[] args) {
 		SpringApplication.run(ShouldIClickThisApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins(unblockList);
+			}
+		};
 	}
 
 }
